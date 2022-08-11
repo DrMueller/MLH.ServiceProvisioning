b869a3860bace972df251ae9e79d831fd6c93d91
Matthias Müller <matthias.mueller@trivadis.com>
2022-08-11 21:36:23 +0200

    WIP

---
+++ Sources/TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mmu.Mlh.ServiceProvisioning.TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
